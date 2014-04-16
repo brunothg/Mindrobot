333829e94a6c149381bdff06af89dcc8cba4256d
Marvin Bruns <Marvin Bruns@n-marvin>
2014-04-16 21:38:21 +0200

    MindTalk: -  DemoRun entfernt

--- src/main/java/de/bno/mindrobot/gui/MindTalk.java
+++ src/main/java/de/bno/mindrobot/gui/MindTalk.java
@@ -8,48 +8,6 @@ public class MindTalk implements Parser {
 	public void run(RobotControl ctrl) {
 		running = true;
 
-		ctrl.moveForwards();
-		ctrl.moveForwards();
-
-		ctrl.turnRight();
-		ctrl.moveForwards();
-		ctrl.moveForwards();
-		ctrl.moveBackwards();
-		ctrl.moveBackwards();
-		ctrl.moveBackwards();
-		ctrl.moveBackwards();
-
-		ctrl.turnRight();
-		ctrl.moveBackwards();
-		ctrl.moveBackwards();
-
-		ctrl.moveForwards();
-		ctrl.moveForwards();
-
-		ctrl.turnRight();
-		ctrl.moveBackwards();
-		ctrl.moveBackwards();
-
-		ctrl.turnLeft();
-		ctrl.moveBackwards();
-		ctrl.moveBackwards();
-
-		if (running) {
-			ctrl.turnRight();
-			ctrl.moveBackwards();
-			ctrl.moveBackwards();
-			ctrl.moveForwards();
-			ctrl.moveForwards();
-
-			ctrl.turnLeft();
-			ctrl.moveForwards();
-			ctrl.moveForwards();
-
-			ctrl.turnLeft();
-			ctrl.moveForwards();
-			ctrl.moveForwards();
-			ctrl.moveBackwards();
-		}
 		running = false;
 	}
 
